548a87f90102a0ded0d2952cfb3c8d6d27430483
tanhtanh1505 <tanhtanh1505@gmail.com>
2023-05-13 05:03:44 +0700

    fix: Open by click file

--- extension.js
+++ extension.js
@@ -5,62 +5,15 @@ const opn = require("opn");
 
 function activate(context) {
   let disposable = vscode.commands.registerCommand("live-page.open", function () {
-    const workspacePath = vscode.workspace.rootPath;
-    if (!workspacePath) {
-      vscode.window.showErrorMessage("Không tìm thấy đường dẫn thư mục đang làm việc!");
-      return;
+    let editor = vscode.window.activeTextEditor;
+    if (editor.document.languageId === "html") {
+      let fileUri = vscode.Uri.file(editor.document.fileName);
+      console.log(fileUri);
+      let url = fileUri.path;
+      opn(url);
+    } else {
+      vscode.window.showInformationMessage("Please open an HTML file to open in browser.");
     }
-
-    // Read all files in workspace
-    fs.readdir(workspacePath, (err, files) => {
-      if (err) {
-        vscode.window.showErrorMessage("Đã xảy ra lỗi khi đọc thư mục: " + err.message);
-        return;
-      }
-
-      // Get all html files
-      const htmlFiles = files.filter((file) => path.extname(file) === ".html");
-      if (htmlFiles.length === 0) {
-        vscode.window.showWarningMessage("Không tìm thấy file HTML trong thư mục đang làm việc!");
-        return;
-      }
-
-      //find config file in workspace name .livepage.json
-      const configFiles = fs.readFileSync(path.join(workspacePath, "lpconfig.json"), "utf8");
-
-      let configure = {
-        path: `file://${workspacePath}`,
-      };
-
-      if (configFiles) {
-        const config = JSON.parse(configFiles);
-
-        configure = {
-          path: config.path || "",
-        };
-      }
-
-      // Show quick pick to select file
-      const items = htmlFiles.map((file) => {
-        return {
-          label: file,
-          description: "File HTML",
-          detail: path.join(workspacePath, file),
-        };
-      });
-
-      vscode.window.showQuickPick(items).then((selected) => {
-        if (selected) {
-          // Open file in editor and open in browser
-          vscode.workspace.openTextDocument(selected.detail).then((document) => {
-            vscode.window.showTextDocument(document);
-
-            const url = `${configure.path}/${selected.label}`;
-            opn(url);
-          });
-        }
-      });
-    });
   });
 
   context.subscriptions.push(disposable);
